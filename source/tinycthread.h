feece1ff3b05ce5d17b8cba873f1ddeaaada976b
Haikuno <mopprower@gmail.com>
2026-02-26 23:20:23 -0300

    add an option to force use of c11 threads

--- source/tinycthread.h
+++ source/tinycthread.h
@@ -64,7 +64,7 @@ extern "C" {
 #if !defined(_TTHREAD_PLATFORM_DEFINED_)
   #if defined(_WIN32) || defined(__WIN32__) || defined(__WINDOWS__)
     #define _TTHREAD_WIN32_
-  #elif defined(__DREAMCAST__) || defined(__GAMECUBE__)
+  #elif defined(__DREAMCAST__) || defined(__GAMECUBE__) || defined(TINYCTHREAD_FORCE_C11)
     #define _TTHREAD_CTHREADS_
     #include <threads.h>
   #else
